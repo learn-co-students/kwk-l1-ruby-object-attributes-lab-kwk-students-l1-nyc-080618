a5baa471b01cac1ad945577e656d091a77b0b448
YingqiTao <yingqitao2000@gmail.com>
2018-08-09 20:09:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # dog.rb
+class Dog 
+  def name 
+    @name
+  end
+  
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+  def breed 
+    @breed 
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,18 @@
 # person.rb
+class Person 
+  def name 
+    @name 
+  end
+  
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(person_job)
+    @job = person_job
+  end
+end

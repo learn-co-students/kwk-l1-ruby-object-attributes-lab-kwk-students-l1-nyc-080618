c7972470145d04110e48f357d3f8a09ea982ca21
jahnavisen <jahnavi.sen02@gmail.com>
2018-08-10 14:24:13 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,45 @@
-# dog.rb
+# # # dog.rb
+# class Dog 
+#   def initialize (name, breed)
+#     @name = name 
+#     @breed = breed 
+#   end
+#   def name= (new_name)
+#     @name = new_name
+#   end 
+#   def breed= (new_breed)
+#     @breed = new_breed
+#   end 
+# end 
+
+
+# class Dog 
+#   def initialize (name) 
+#     @name = name 
+#   end 
+#   def name 
+#     return @name 
+#   end 
+# end 
+# dog = Dog.new 
+# puts dog.name 
+
+
+class Dog 
+ 
+  def name 
+    @name 
+  end 
+  def name=(new_name)
+    @name=new_name 
+  end 
+  def breed 
+    @breed 
+  end 
+  def breed=(new_breed)
+    @breed=new_breed 
+  end 
+
+end 
+
+
